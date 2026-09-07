0d9bb1bf47c2afb5609d9675e83d677a58f483f5
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-09-07 13:28:48 -0300

    Account adicionada

---
+++ server/source/gameplay/account/account.gd
@@ -0,0 +1,43 @@
+extends Node
+class_name Account
+
+
+var id: int
+var email: String
+var password: String
+var access_at: int
+var created_at: int
+var updated_at: int
+
+var character: Character
+
+
+func setup(
+	id: int,
+	email: String,
+	password: String,
+	access_at: int,
+	created_at: int,
+	updated_at: int
+) -> void:
+	self.id = id
+	self.email = email
+	self.password = password
+	self.access_at = access_at
+	self.created_at = created_at
+	self.updated_at = updated_at
+
+
+func has_character() -> bool:
+	return character != null
+
+
+func select_character(character: Character) -> void:
+	if character.account_id != id:
+		return
+
+	self.character = character
+
+
+func clear_character() -> void:
+	character = null
